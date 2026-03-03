26ec0c9ea2dd6caa9ec47fbf3d84be22ed558a7d
hrishitac <hrishita.chakrabarti@gmail.com>
2026-03-03 17:14:24 +0100

    Issue detected with SERP logging, when returning from an external website, the logger does not log the SERP when page number > 1

--- search-app/static/logger.js
+++ search-app/static/logger.js
@@ -10,34 +10,6 @@
         });
     }
 
-    // const historyTracker = {
-    //     history: [],
-        
-    //     init() {
-    //         const storedHistory = localStorage.getItem('broswerHistory');
-    //         this.history = storedHistory ? JSON.parse(storedHistory) : [];
-    //     },
-
-    //     addPage(url) {
-    //         this.history.push(url);
-    //         localStorage.setItem('broswerHistory', JSON.stringify(this.history));
-    //     },
-
-    //     getLastPageVisited() {
-    //         if (this.history.length === 0){
-    //             return null
-    //         }
-    //         else {
-    //             return this.history[this.history.length - 2];
-    //         }
-    //     },
-
-    //     removePages(){
-    //         this.history.splice(-3,3);
-    //         localStorage.setItem('browserHistory', JSON.stringify(this.history));
-    //     }
-    // }
-
     const logger = {
         sessionID: null,
         logs: [],
@@ -82,10 +54,10 @@
             else return;
         },
 
-        // removeHistory(){
-        //     this.historyTracker.splice(this.historyTracker.length-2, 2);
-        //     localStorage.setItem('browserHistory', JSON.stringify(this.historyTracker));
-        // },
+        removeHistory(){
+            this.historyTracker.splice(this.historyTracker.length-2, 2);
+            localStorage.setItem('browserHistory', JSON.stringify(this.historyTracker));
+        },
 
         getHistory() {
             return JSON.stringify(this.historyTracker);
@@ -108,7 +80,6 @@
                     console.log('Logs successfully sent to server.');
                     localStorage.removeItem('sessionLogs');
                     localStorage.removeItem('sessionID');
-                    localStorage.removeItem('wentBack');
                     localStorage.removeItem('browserHistory');
                     this.logs = [];
                     this.historyTracker = [];
@@ -120,9 +91,7 @@
     };
 
     logger.init();
-    // historyTracker.init();
     window.studyLogger = logger;
-    // window.browserHistoryTracker = historyTracker;
 })();
 
 const idform = document.getElementById("enter-id-form");
@@ -139,21 +108,6 @@ if (taskbtn) {
         studyLogger.logEvent("TaskStarted");
     });
 }
-
-// const taskbtn = document.getElementById("task-btn")
-// if (taskbtn) {
-//     const wrapper = document.getElementById("task-wrapper");
-//     const userId = wrapper.getAttribute("data-user-id");
-//     const tasknum = wrapper.getAttribute("data-task-num");
-//     const topictext = wrapper.getAttribute("data-topic");
-//     taskbtn.addEventListener("click", () => {
-//         studyLogger.logEvent("TaskStarted", {
-//             task: tasknum,
-//             uid: userId,
-//             topic: topictext
-//         });
-//     });
-// }
 
 const searchbox = document.getElementById("search-box")
 if (searchbox) {
@@ -172,29 +126,8 @@ if (searchbar) {
     }); 
 }
 
-const goBackBtn = document.getElementById("go-back-btn");
-if (goBackBtn){
-    goBackBtn.addEventListener("click", ()=>{
-        localStorage.setItem('wentBack', 1);
-    });
-}
-
 const searchResults = document.querySelectorAll("article.content-section");
-if(searchResults){
-    const firstResult = document.querySelector("article.content-section");
-    const query = firstResult.getAttribute("query");
-    const  page = firstResult.getAttribute("page");
-    const searchAppLocation = page=="1" ? window.location.href + "?query="+query+"&page=1" : window.location.href;
-    if(studyLogger.checkHistory(searchAppLocation)){
-        studyLogger.logEvent("wentBack", {
-            "query": query,
-            "page": page
-        });
-        // studyLogger.removeHistory();
-        studyLogger.addHistory(searchAppLocation);
-    }
-    else{
-        studyLogger.addHistory(searchAppLocation);
+    if(searchResults){
         searchResults.forEach(result => {
             const query = result.getAttribute("query");
             const docid = result.getAttribute("base_ir");
@@ -202,20 +135,110 @@ if(searchResults){
             const page = result.getAttribute("page");
             const url = document.getElementById(`abstract-link-${rank}`).getAttribute("href");
             const searchAppLocation = page==="1" ? window.location.href + "?query="+query+"&page=1" : window.location.href;
-            // if (rank=="1") studyLogger.addHistory(searchAppLocation);
             
             studyLogger.logEvent("searchResultGenerated", {
+                        query: query,
+                        docid: docid,
+                        rank: rank,
+                        page: page,
+                        url: url,
+                        windowLocation: searchAppLocation,
+                        // history: studyLogger.getHistory(),
+                });
+
+            result.addEventListener("mouseenter", ()=>{           
+                studyLogger.logEvent("cursorEnteredSnippet", {
                     query: query,
                     docid: docid,
                     rank: rank,
                     page: page,
                     url: url,
                     windowLocation: searchAppLocation,
-                    history: studyLogger.getHistory(),
+                    // history: studyLogger.getHistory(),
                 });
-        });
-    }
+            });
+
+            result.addEventListener("mouseleave", ()=>{            
+                studyLogger.logEvent("cursorLeftSnippet", {
+                    query: query,
+                    docid: docid,
+                    rank: rank,
+                    page: page,
+                    url: url,
+                    windowLocation: searchAppLocation,
+                    // history: studyLogger.getHistory(),
+                });
+            });
+    });
 }
+
+
+    
+    // const firstResult = document.querySelector("article.content-section");
+    // const query = firstResult.getAttribute("query");
+    // const  page = firstResult.getAttribute("page");
+    // const searchAppLocation = page=="1" ? window.location.href + "?query="+query+"&page=1" : window.location.href;
+
+    // if(studyLogger.checkHistory(searchAppLocation)){
+    //     studyLogger.logEvent("wentBack", {
+    //         "query": query,
+    //         "page": page
+    //     });
+    //     studyLogger.removeHistory();
+    //     studyLogger.addHistory(searchAppLocation);
+    // }
+    // else{
+        // studyLogger.addHistory(searchAppLocation);
+        // searchResults.forEach(result => {
+        //     const query = result.getAttribute("query");
+        //     const docid = result.getAttribute("base_ir");
+        //     const rank = result.id.split("-")[1];
+        //     const page = result.getAttribute("page");
+        //     const url = document.getElementById(`abstract-link-${rank}`).getAttribute("href");
+        //     const searchAppLocation = page==="1" ? window.location.href + "?query="+query+"&page=1" : window.location.href;
+            
+        //     studyLogger.logEvent("searchResultGenerated", {
+        //             query: query,
+        //             docid: docid,
+        //             rank: rank,
+        //             page: page,
+        //             url: url,
+        //             windowLocation: searchAppLocation,
+        //             history: studyLogger.getHistory(),
+        //         });
+        // });
+    // }
+
+    // searchResults.forEach(result=>{
+    //     const query = result.getAttribute("query");
+    //     const docid = result.getAttribute("base_ir");
+    //     const rank = result.id.split("-")[1];
+    //     const page = result.getAttribute("page");  
+        
+//         result.addEventListener("mouseenter", ()=>{
+//             studyLogger.logEvent("cursorEnteredSnippet", {
+//                 query: query,
+//                 docid: docid,
+//                 rank: rank,
+//                 page: page,
+//                 windowLocation: searchAppLocation,
+//                 history: studyLogger.getHistory(),
+//             });
+//         });
+
+//         result.addEventListener("mouseleave", ()=>{
+//             studyLogger.logEvent("cursorLeftSnippet", {
+//                 query: query,
+//                 docid: docid,
+//                 rank: rank,
+//                 page: page,
+//                 windowLocation: searchAppLocation,
+//                 history: studyLogger.getHistory(),
+//             });
+//         });
+//     });
+
+// }
 // if (localStorage.getItem('wentBack')){
 //     const firstResult = document.querySelector("article.content-section");
 //     const query = firstResult.getAttribute("query");
@@ -263,39 +286,12 @@ if(searchResults){
     // }
 // }
 
-if(searchResults){
-    searchResults.forEach(result=>{
-        const query = result.getAttribute("query");
-        const docid = result.getAttribute("base_ir");
-        const rank = result.id.split("-")[1];
-        const page = result.getAttribute("page");
-        result.addEventListener("mouseenter", ()=>{
-            studyLogger.logEvent("cursorEnteredSnippet", {
-                query: query,
-                docid: docid,
-                rank: rank,
-                page: page
-            });
-        });
-
-        // result.addEventListener("mouseleave", ()=>{
-        //     studyLogger.logEvent("cursorLeftSnippet", {
-        //         query: query,
-        //         docid: docid,
-        //         rank: rank,
-        //         page: page
-        //     });
-        // });
-    });
-}
-
 
 const resultLinks = document.querySelectorAll("a.result-link");
 if (resultLinks) {
     // const serpContainer = document.getElementsByClassName("container-info");
     resultLinks.forEach(link => {
         link.addEventListener("click", (e) => {
-            localStorage.setItem('wentBack', 1);
             const rank = link.id.split("-")[2];
             const url = link.getAttribute("href");
             const snippet = document.getElementById(`result-${rank}`);
@@ -308,8 +304,8 @@ if (resultLinks) {
                 rank: rank,
                 page: page,
                 url: url,
-                searchAppLocation: url,
-                history: studyLogger.getHistory(),
+                windowLocation: url,
+                // history: studyLogger.getHistory(),
             });
 
         });
@@ -344,66 +340,6 @@ homeButton.addEventListener("click", ()=>{
     studyLogger.logEvent("wentBackHome");
 });
 
-// const args = document.querySelectorAll(".toggle-abstract");
-// if (args) {
-//     document.querySelectorAll(".toggle-abstract").forEach(toggle => {
-//             toggle.addEventListener("click", () => {
-//                 const index = toggle.getAttribute("data-index");
-//                 const rank = toggle.getAttribute("data-rank");
-//                 const full = document.getElementById(`abstract-full-${index}`);
-//                 const did = toggle.getAttribute("did");
-
-//                 if (full.classList.contains("d-none")) {
-//                     studyLogger.logEvent("toggleArgument", {
-//                         rank: rank,
-//                         doc: did,
-//                         action: "expand",
-//                         doclen: full.textContent.trim().length
-//                     }); 
-//                 } else {
-//                     studyLogger.logEvent("toggleArgument", {
-//                         rank: rank,
-//                         doc: did,
-//                         action: "reduce",
-//                         doclen: full.textContent.trim().length
-//                     }); 
-//                 }
-//             });
-//     });
-// }
-
-// const pcbuttons = document.querySelectorAll('button[data-type="pro"], button[data-type="con"]');
-// if(pcbuttons) {
-//     pcbuttons.forEach(btn => {
-//         btn.addEventListener("click", () => {
-//             const type = btn.getAttribute("data-type")
-//             const rank = btn.getAttribute("data-rank");
-//             const index = btn.getAttribute("data-index");
-//             const full = document.getElementById(`abstract-full-${index}`);
-//             const did = btn.getAttribute("did");
-//             const currentState = argumentSelections[did];
-//             if (currentState === type) {
-//                 studyLogger.logEvent("StanceClicked", {
-//                 rank: rank,
-//                 stance: type,
-//                 action: "chooseStance",
-//                 doc: did,
-//                 doclen: full.textContent.trim().length
-//             });
-//             } else {
-//                 studyLogger.logEvent("StanceClicked", {
-//                 rank: rank,
-//                 stance: type,
-//                 action: "removeStance",
-//                 doc: did,
-//                 doclen: full.textContent.trim().length
-//             });
-//             }
-            
-//         })
-//     })
-// }
-
 
 endtask = document.getElementById("end-task-btn")
 if (endtask) {
@@ -435,18 +371,4 @@ if (endno) {
         studyLogger.logEvent("TaskContinued");
     });
 }
-
-// toggleSaved = document.getElementById('toggleSavedLink');
-// if (toggleSaved) {
-//     toggleSaved.addEventListener("click", () => {
-//         studyLogger.logEvent("toggleSavedDocumentsClicked", {
-//         clicked: getStatus(document.getElementById('savedTitles'))
-//         }); 
-//     });
-
-//     function getStatus(doclist) {
-//     if (doclist.style.display == 'none') return "hidden";
-//     else return "shown";
-//     }
-// }
 
